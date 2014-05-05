770a80cf49323518b137910dd071d58c644b3628
Fernando Rodriguez Sela <frsela@tid.es>
2014-05-05 15:00:28 +0200

    First bugzilla console viewer script

---
+++ viewbug.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+var https = require('https');
+
+if (process.argv.length >= 3) {
+  queryBug(process.argv[2]);
+} else {
+  require('child_process').exec('git branch | grep "*" | tail -c 7', function(error, stdout, stderr) {
+    queryBug(stdout);
+  });
+}
+
+function queryBug(bugNumber) {
+  console.log('Looking for bug ' + bugNumber);
+
+  req = https.request('https://bugzilla.mozilla.org/rest/bug/' + bugNumber, function(res) {
+    var bugData = '';
+    res.on('data', function(d) {
+      bugData = bugData + d;
+    });
+    res.on('end', function(d) {
+      if (d) {
+        bugData = bugData + d;
+      }
+      try {
+        bugData = JSON.parse(bugData);
+        showBug(bugData);
+      } catch(e) {
+        console.log('Error: ' + e);
+      }
+    });
+  });
+  req.on('error', function(e) {
+    console.log('Error: ' + e);
+  });
+  req.end();
+}
+
+function showBug(bugData) {
+  console.log('Bug number: ' + bugData.bugs[0].id);
+  console.log('Summary: ' + bugData.bugs[0].summary);
+  console.log('Status: ' + bugData.bugs[0].status);
+  console.log('Assigned to: ' + bugData.bugs[0].assigned_to);
+  console.log('Created on: ' + bugData.bugs[0].creation_time);
+  console.log('Changed on: ' + bugData.bugs[0].last_change_time);
+}
